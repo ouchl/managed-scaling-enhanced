32d71e8ab25fbb7b4248bff46944955de47cb06e
Liang Hao <habliang@amazon.com>
2024-04-10 02:26:19 +0800

    update

--- managed_scaling_enhanced/scale.py
+++ managed_scaling_enhanced/scale.py
@@ -48,10 +48,10 @@ def scale_in(cluster: Cluster, dry_run: bool = False) -> bool:
             logger.info(
                 f'New target OD capacity is {new_od_capacity}. New target spot capacity is {new_spot_capacity}.')
             new_max_units = target_capacity + cluster.managed_scaling_policy_max_core_units + 1
-            logger.info(f'Setting managed scaling policy max units to {new_max_units}.')
+            logger.info(f'Modifying managed scaling policy max units from {cluster.managed_scaling_policy_max_units} to {new_max_units}.')
             cluster.modify_scaling_policy(max_units=new_max_units)
-            logger.info(f'Setting task fleet target OD capacity to {new_od_capacity}.')
-            logger.info(f'Setting task fleet target spot capacity to {new_spot_capacity}.')
+            logger.info(f'Modifying task fleet target OD capacity from {cluster.task_target_od_capacity} to {new_od_capacity}.')
+            logger.info(f'Modifying task fleet target spot capacity from {cluster.task_target_spot_capacity} to {new_spot_capacity}.')
             if not dry_run:
                 emr_client.put_managed_scaling_policy(ClusterId=cluster.id,
                                                       ManagedScalingPolicy=cluster.managed_scaling_policy)
